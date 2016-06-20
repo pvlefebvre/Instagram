2474eacb130dbd27c4873fefe97f440a9dd9bd73
Tyler Italiano <tyleritaliano@Tylers-MacBook-Air.local>
2016-06-20 16:09:48 -0500

    Constrainted and organzied

---
+++ Instagram/PhotoCommentsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCommentsViewController.swift
+//  Instagram
+//
+//  Created by Tyler Italiano on 6/19/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCommentsViewController: UIViewController {
+    @IBOutlet weak var commentPhoto: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    
+
+}
---
+++ ProfileViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileViewController.swift
+//  Instagram
+//
+//  Created by Tyler Italiano on 6/19/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+//    UICollectionViewDelegate, UICollectionViewDataSource, UICollectionView, UICollectionViewCell, UICollectionViewFlowLayout
+    @IBAction func editProfileButton(sender: AnyObject) {
+    }
+    @IBOutlet weak var postsCounterLabel: UILabel!
+    @IBOutlet weak var followersCountLabel: UILabel!
+    @IBOutlet weak var followingCounterLabel: UILabel!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileDescriptionLabel: UITextView!
+    @IBOutlet weak var profileCollectionView: UICollectionView!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    let cell = "ProfilePhotoCell"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilePicture.layer.cornerRadius = profilePicture.frame.size.width/2
+        profilePicture.clipsToBounds = true
+        
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ProfilePhotoCell", forIndexPath: indexPath) 
+        cell.backgroundColor = UIColor.redColor()
+        return cell
+        
+        
+    }
+    
+    
+}
