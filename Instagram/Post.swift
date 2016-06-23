a34e70e86ea6aa710f8464358c7f741f464e30a8
Lance Russ <lance.r.russ@gmail.com>
2016-06-23 09:36:06 -0500

    posting functionality

--- Instagram/Post.swift
+++ Instagram/Post.swift
@@ -11,12 +11,23 @@ import UIKit
 class Post: NSObject {
     
     let comments: [Comment]? = nil
-    let account: Account?
-    let caption: String? = nil
-    let location: String? = nil
-    let image: UIImage? = nil
+    let account: Account? = nil
+    var userID: NSString
+    var caption: NSString
+    var location: NSString
+    var image: NSString
+    var likeCount: NSNumber = 0
+    var objectId: String? = nil
+//    var dateString: NSString = ""
     
-    init(account: Account) {
-        self.account = account
+    init(userID: NSString, caption: NSString, image: NSString, location: NSString) {
+        
+        self.caption = caption
+        self.image = image
+        self.location = location
+        self.userID = userID
+//        self.likeCount = 0
+//        self.objectId = nil
+        
     }
 }
